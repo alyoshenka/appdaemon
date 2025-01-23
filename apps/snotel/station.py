4c8322fc4f42cdbed3c77d8dbe104e1c3190a3a6
Jay <alexibmost@gmail.com>
2025-01-23 15:09:26 -0700

    using my own data

--- apps/snotel/station.py
+++ apps/snotel/station.py
@@ -5,44 +5,49 @@ import datetime
 from data_request import *
 from data_manipulation import *
 
-DEFAULT_DATA = {
-    'station_id': 0,
-    'station_name': 'Bozeman',  
-    'last_updated': 'Never',
-    'temperature': 100,
-    'snow_depth': 100,
-    '24_hour': {
-        'max_temp': 100,
-        'min_temp': 100,
-        'delta_swe': 100,
-        'delta_depth': 100,
-    },
-    '72_hour': {
-        'max_temp': 100,
-        'min_temp': 100,
-        'delta_swe': 100,
-        'delta_depth': 100,
-    },
-    '168_hour': {
-        'max_temp': 100,
-        'min_temp': 100,
-        'delta_swe': 100,
-        'delta_depth': 100,
-    }
-}
-
 
 class SnotelStation(hass.Hass):
 
+    def default_data(self): 
+        return {
+            'station_id': self.args['station_id'],
+            'station_link': make_station_url(self.args['station_id']),
+            'station_name': '#' + str(self.args['station_id']),  
+            'station_info': 'SNOTEL site',
+            'last_updated': 'Never',
+            'temperature': '-',
+            'snow_depth': '-',
+            '24_hour': {
+                'max_temp': '-',
+                'min_temp': '-',
+                'delta_swe': '-',
+                'delta_depth': '-',
+            },
+            '72_hour': {
+                'max_temp': '-',
+                'min_temp': '-',
+                'delta_swe': '-',
+                'delta_depth': '-',
+            },
+            '168_hour': {
+                'max_temp': '-',
+                'min_temp': '-',
+                'delta_swe': '-',
+                'delta_depth': '-',
+            }
+        }
+
     def initialize(self):
         self.log('Snotel Station initialized')
-        self.log('Station id: ' + str(self.args['station_id']))
-        self.set_state(entity_id='sensor.test_snotel_data', state=self.args['station_id'], attributes=DEFAULT_DATA)
-
+        station_id = str(self.args['station_id'])
+        self.log('Station id: ' + station_id)
+        self.set_state(entity_id='sensor.snotel_station_' + station_id, state=station_id, attributes=self.default_data())
         self.update()
 
     def terminate(self):
-        self.log('Snotel Station terminated')
+        station_id = str(self.args['station_id'])
+        self.set_state(entity_id='sensor.snotel_station_' + station_id, state=station_id, attributes=self.default_data())
+        self.log('Snotel Station ' + station_id + ' terminated')
 
     def update(self):
         self.log('Updating data: ' + str(self.args['station_id']))
@@ -52,10 +57,14 @@ class SnotelStation(hass.Hass):
             soup = make_soup(res.text)
             self.log('Updating data for ' + get_report_info(soup)['site'])
 
+            info = get_report_info(soup)
             data = get_report_data(soup)
             df = arr_to_df(data)
 
-            new_data = DEFAULT_DATA
+            new_data = self.default_data()
+
+            new_data['station_name'] = info['site']
+            new_data['station_info'] = info['info']
 
             new_data['last_updated'] = datetime.datetime.now()
             new_data['temperature'] = get_temp(df)
@@ -79,7 +88,7 @@ class SnotelStation(hass.Hass):
             new_data['168_hour']['delta_swe'] = delta_swe(df)
             new_data['168_hour']['delta_depth'] = delta_depth(df)
 
-            self.set_state(entity_id='sensor.test_snotel_data', state='ON', attributes=new_data)
+            self.set_state(entity_id='sensor.snotel_station_' + str(self.args['station_id']), attributes=new_data)
 
             self.log('Done updating Station ' + str(self.args['station_id']))
 
